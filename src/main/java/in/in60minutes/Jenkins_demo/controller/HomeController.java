c0007b8c088ce1a470d1f84f0dc4562b77fae2cd
Raju Harsha <71176027+Harsha146@users.noreply.github.com>
2026-05-04 10:58:28 +0530

    edit controller file

--- src/main/java/in/in60minutes/Jenkins_demo/controller/HomeController.java
+++ src/main/java/in/in60minutes/Jenkins_demo/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home(){
         return "Hello Spring Boot ...";
     }
+
+    @GetMapping("/raj")
+    public String raj(){
+        return "Hello Raj ...";
+    }
 }
